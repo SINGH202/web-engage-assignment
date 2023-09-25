31d624e094be4485fdb5132a9b98b82982915fab
ANURAG SINGH <apk.anurag.singh@gmail.com>
2023-09-25 17:47:46 +0530

    Added UI.

---
+++ onsite-survey/index.js
@@ -0,0 +1,5 @@
+const submitButton = document.getElementById("submitButton");
+const mobileInp = document.getElementById("mobile");
+const emailInp = document.getElementById("email");
+const countryCodeInp = document.getElementById("countryCode");
+
